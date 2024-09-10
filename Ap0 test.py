2f01d002ea8775d7d17188095ead08593d2d74d7
cody <cafunes-nieves@loyola.edu>
2024-09-09 23:57:08 -0400

    updated 11:56pm 9/9/2024 tested and working

--- Ap0 test.py
+++ Ap0 test.py
@@ -1,10 +1,10 @@
-print ("Enter Investment Amount:", end=' ')
+print ('Enter Investment Amount:', end=' ')
 Initial_balance = int(input())
-print ("intrest percent", end=' ')
-I = int(input())
-
-N = 7
+print ('Enter Intrest Percent:', end=' ')
+I = float(input())
+print('Enter Years To Grow:', end=' ')
+N = int(input())
 
 balance: float = ((Initial_balance*(1+I)**N)-Initial_balance)
 
-print ("Yvonne's Compound interest after 7 years:", balance)
+print ('Compound interest after',N, 'years:', balance)
