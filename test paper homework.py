f8a16aa993e4c8da4773ee7de69371e737be8b2e
cody <cafunes-nieves@loyola.edu>
2024-09-23 14:34:32 -0400

    67

--- test paper homework.py
+++ test paper homework.py
@@ -5,13 +5,8 @@ mary_credits = int(input("Please enter the number of Mary's credits:"))
 
 print("Are bob and mary taking at least 15 credits:")
 
-totalcredit = "Yes"
-if bob_credits and mary_credits >= 15:
-    totalcredit= "Yes"
-elif bob_credits and mary_credits < 15:
-    totalcredit= "No"
+totalcredit = "No"
+if bob_credits >= 15 and mary_credits >= 15:
+        totalcredit = "Yes"
 
 print(totalcredit)
-
-
-23
--- test.py
+++ test.py
@@ -1,32 +1,32 @@
-score1=int(input("Player 1 Enter Your Number:"))
-score2=int(input("Player 2 Enter Your Number:"))
-
-Player1Win: int = 0
-Player2Win: int = 0
-
-Player1scoreboard = 0
-Player2scoreboard = 0
-
-print("Player 1 Number was:", score1)
-print("Player 2 Number was:", score2)
-print("The Winner is:")
-
-if score1 > score2:
-    Player1scoreboard = Player1Win + 1
-    print("Player 1 Wins")
-elif score1 < score2:
-    Player2scoreboard = Player2Win + 1
-    print("Player 2 Wins")
-if score1 == score2:
-    print("Tie")
-print("The score is:\n", "Player1:", Player1scoreboard, '\n', "Player2:", Player2scoreboard)
-
 def loop():
+    score1 = int(input("Player 1 Enter Your Number:"))
+    score2 = int(input("Player 2 Enter Your Number:"))
+
+    Player1Win: int = 0
+    Player2Win: int = 0
+
+    Player1scoreboard = 0
+    Player2scoreboard = 0
+
+    print("Player 1 Number was:", score1)
+    print("Player 2 Number was:", score2)
+    print("The Winner is:")
+
+    if score1 > score2:
+        Player1scoreboard = Player1Win + 1
+        print("Player 1 Wins")
+    elif score1 < score2:
+        Player2scoreboard = Player2Win + 1
+        print("Player 2 Wins")
+    if score1 == score2:
+        print("Tie")
+    print("The score is:\n", "Player1:", Player1scoreboard, '\n', "Player2:", Player2scoreboard)
+
     restart = input("Would you like to play again:")
     if restart == "yes":
         loop()
     if restart == "no":
-        print("Thank you for playing")
+        print("Thank you for playing!")
 loop()
 
 
