3164d638d519d3b67c366148451c0699d31c379c
cody <cafunes-nieves@loyola.edu>
2024-09-30 12:49:12 -0400

    test added

--- game test AP1.py
+++ game test AP1.py
@@ -1,3 +1,7 @@
+import tkinter
+3
+
+
 Next_text = input(print("You are a worrier walking in a forest and as you are walking a green witch siting in a bush steals your pouch of gold and says"))
 input("Hit the Enter Key to contine:")
 which_hand = input("GREEN WITCH - 'Which hand of the witch will it be' As she hold her hand out wanting for you to choose which hand your gold is in. Will you choose thr right or left? ")
